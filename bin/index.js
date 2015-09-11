8f1d121721f5db9a34ea6ca412582bf31f9ca891
matthesrieke <m.rieke@52north.org>
2015-09-11 11:26:15 +0200

    use original WMS url

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 
-var cluster = require('cluster');
+//var cluster = require('cluster');
+//
+//var type;
+//if (cluster.isMaster) {
+//  type = 'master';
+//} else {
+//  type = process.env.WORKER_TYPE;
+//}
+//
+//require('../lib/workers/' + type)();
 
-var type;
-if (cluster.isMaster) {
-  type = 'master';
-} else {
-  type = process.env.WORKER_TYPE;
-}
 
-require('../lib/workers/' + type)();
+var harvester = require('../lib/harvester');
+var config = require('../lib/config');
 
-
-
+harvester.Manager.create(config.harvester);
